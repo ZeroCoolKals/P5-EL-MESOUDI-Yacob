4c166cb1d25bd4cc5f9ffd04fad309a497f3a2e0
ZeroCoolKals <kalsballer@gmail.com>
2022-04-10 23:16:31 +0000

    fix bug pour la modif de la quantité sur le meme produit

--- front/scripts/cart.js
+++ front/scripts/cart.js
@@ -1,12 +1,12 @@
 
 let getProductsInArray = JSON.parse(localStorage.getItem("products"));
-let totalQuantityValue = 0;
+/* let totalQuantityValue = 0;
 const totalQuantity = document.getElementById("totalQuantity");
 
 const totalPrice = document.getElementById("totalPrice");
 let totalPriceValue = 0;
 
-
+ */
 
 
 ArrayDetails();
@@ -93,8 +93,8 @@ async function ArrayDetails() {
             quantityDetails.max = '100';
             quantityDetails.setAttribute("value", productInCart.quantity);
 
-            totalQuantityValue = totalQuantityValue + parseInt(productInCart.quantity);
-            totalQuantity.innerHTML = totalQuantityValue;
+            //totalQuantityValue = totalQuantityValue + parseInt(productInCart.quantity);
+            //totalQuantity.innerHTML = totalQuantityValue;
 
             modifyQuantityInLsAndPriceInDom();
             function modifyQuantityInLsAndPriceInDom() {
@@ -102,11 +102,12 @@ async function ArrayDetails() {
                     const euroFormat = new Intl.NumberFormat("fr-FR", { style: "currency", currency: "EUR" }).format(getPrice * quantityDetails.value)
                     productPrice.innerText = euroFormat;
                     getProductsInArray.forEach(element => {
-                        if (element.id === productInCart.id) {
+                        if (element.id === productInCart.id && productInCart.color == element.color) {
                             element.quantity = e.target.value;
                         }
                     });
                     localStorage.setItem("products", JSON.stringify(getProductsInArray));
+                    calculTotaux()
                 });
             };
 
@@ -125,21 +126,125 @@ async function ArrayDetails() {
                     parent.removeChild(quantityDetails.closest(".cart__item"));
                     getProductsInArray.splice(indexOfProductInCart, 1);
                     localStorage.setItem("products", JSON.stringify(getProductsInArray));
+                    calculTotaux()
                 })
             }
 
         }
     })
+    calculTotaux()
     checkForm();
 
 }
 
 
+function checkForm() {
+    /** Cibler les diférents inputs du formulaire */
+    const order = document.getElementById("order");
+    const firstName = document.getElementById("firstName");
+    const lastName = document.getElementById("lastName");
+    const address = document.getElementById("address");
+    const city = document.getElementById("city");
+    const email = document.getElementById("email");
+
+    //map d'erreur pour chaque input
+    //voir map en javascript (mdn ou autre ressource en ligne)
+    const validError = {
+        firstNameErrorMsg: "error 1",
+        lastNameErrorMsg: "error 2",
+        addressErrorMsg: "error 3",
+        cityErrorMsg: "error 4",
+        emailErrorMsg: "error 5",
+    }
 
 
+    /** Regex des inputs */
+    const validateFirstNameOrLastNameOrCity = /^\w+([-'\s]\w+)?/;
+    const validateAdress = /^[0-9]*[\s]\w+([-'\s]\w)?/;
+    const validateEmail = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+
+    /** Au clic sur le bouton "Commander !", paramétrage des actions efféctuées en fonction des conditions */
+    order.addEventListener('click', (e) => {
+        e.preventDefault();
+
+        /* gerer les erreurs de façon dynamic en recuperant toutes 
+        les div avec la class 'cart__order__form__question' du document  */
+        const form = document.getElementsByClassName("cart__order__form__question");
+
+        //Iterer sur chaque element de form
+        for (var i = 0; i < form.length; i++) {
+            //recuper l'element de type input dans le bloc
+            const formInput = form[i].getElementsByTagName("input").item(0);
+            const textSaisiDansInput = formInput.value;
+
+            //recuper l'element de type p dans le bloc
+            let formErrorMsgP = form[i].getElementsByTagName("p").item(0);
+
+            //Recuperer la valeur de l'attribut 'id'
+            const cle = formErrorMsgP.attributes["id"].value;
+
+            //Recuperer la valeur de l'attribut 'type' (exemple : text, email, number etc)
+            const inputType = formInput.attributes["type"].value;
+
+            // validation selon le type d'input
+            if (inputType == 'text') {
+                // test si la valeur saisi est vali => 
+                if (!validateFirstNameOrLastNameOrCity.test(textSaisiDansInput) /* meme chose si je mets : validateFirstNameOrLastNameOrCity.test(textSaisiDansInput) == false */) {
+                    //recuperer du message d'erreur sur note map (objet) clé-valeur 
+                    const message = validError[cle];
+                    //affichage du message
+                    formErrorMsgP.textContent = message;
+                    return;
+                } else {
+                    formErrorMsgP.textContent = "";
+                }
+            } else if (inputType == "email") {
+                if (!validateEmail.test(textSaisiDansInput)) {
+                    const message = validError[cle];
+                    formErrorMsgP.textContent = message;
+                    return;
+                } else {
+                    formErrorMsgP.textContent = "";
+                }
+            }
+
+        }
+        const order = {
+            contact: {
+                firstName: firstName.value,
+                lastName: lastName.value,
+                address: address.value,
+                city: city.value,
+                email: email.value,
+            },
+            products: getProductsInArray.map(ele => ele.id),
+        };
+
+        send(order);
 
 
+    });
 
+}
+
+function send(order) {
+    fetch("http://localhost:3000/api/products/order", {
+        method: "POST",
+        body: JSON.stringify(order),
+        headers: {
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+    })
+
+        .then((res) => res.json())
+        .then((data) => {
+            //window.location.href = `../html/confirmation.html?id=${data.orderId}`;
+        })
+        .catch(() => {
+            alert("Une erreur est survenue.")
+        });
+}
 
 function checkForm() {
     /** Cibler les diférents inputs du formulaire */
@@ -247,4 +352,63 @@ function send(order) {
         .catch(() => {
             alert("Une erreur est survenue.")
         });
+}
+
+async function calculTotaux() {
+    let totalQuantityValue = 0;
+    let totalPriceValue = 0;
+    const totalQuantity = document.getElementById("totalQuantity");
+    const totalPrice = document.getElementById("totalPrice");
+
+    let getProductsInArray = getLocalStorage();
+    let apiData = await fetch(`http://localhost:3000/api/products`)
+        .then(function (res) {
+            if (res.ok) {
+                return res.json();
+            }
+        })
+        .catch(() => {
+            const errorMessage = document.querySelector("#cart__items");
+            errorMessage.innerHTML = "Oups, l'API semble ne pas fonctionner !";
+            errorMessage.style.textAlign = "center";
+            errorMessage.style.padding = "auto";
+        });
+
+    console.log('getProductsInArray:', getProductsInArray);
+    if (getProductsInArray.length > 0) {
+        getProductsInArray.forEach((localStorageProduct) => {
+            const item = apiData.find(element => element._id == localStorageProduct.id);
+            if (item) {
+                totalQuantityValue +=  parseInt(localStorageProduct.quantity);
+                totalQuantity.innerText = totalQuantityValue;
+                //let totalPriceValue = 0;
+            }
+
+        });
+        console.log('variable:', totalQuantityValue);
+
+
+    }
+}
+
+function getLocalStorage() {
+    return JSON.parse(localStorage.getItem("products"));
+}
+
+function getApiProduct() {
+    let resp = fetch(`http://localhost:3000/api/products`)
+        .then(function (res) {
+            if (res.ok) {
+                return res.json();
+            }
+        })
+
+        .catch(() => {
+            const errorMessage = document.querySelector("#cart__items");
+            errorMessage.innerHTML = "Oups, l'API semble ne pas fonctionner !";
+            errorMessage.style.textAlign = "center";
+            errorMessage.style.padding = "auto";
+            return [];
+        })
+
 }
